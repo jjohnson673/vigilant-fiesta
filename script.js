5b0043f5a35c5ac3e2933703e76e2d1dd4daf994
jjohnson673 <jkjohnson673@gmail.com>
2022-10-23 10:04:27 -0400

    adding in outline notes for JS page

--- script.js
+++ script.js
@@ -1,3 +1,63 @@
-fetch ('https://reqres.in/api/users/23')
-    .then(res => res.json())
-    .then(data => console.log(data))
+//API Key
+
+
+// DOM Elements to Display on page
+
+
+// search city form submission
+
+
+//previously searched city button clicks
+
+
+///requesting current weather for city API
+
+
+//if response was successful
+
+//if response was not successful/ network error
+
+
+//UV index API request
+
+//if error
+
+
+//current weather data display
+
+
+//clear old content
+
+//temperature
+
+//humidity
+
+//wind speed
+
+//list items
+
+
+//uv index
+
+
+//display UV
+
+//5 day forecast API
+
+//is response successful
+
+//if network error
+
+
+//display 5 day forecase
+
+//date
+
+//temp
+
+//humidity
+
+//weather icons
+
+//search button
+
