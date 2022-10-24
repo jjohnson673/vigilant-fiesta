d65ff1b39cc53a704ad7d8ba062a9c156a48e75e
jjohnson673 <jkjohnson673@gmail.com>
2022-10-24 09:31:03 -0400

    adding in 5 day forecast

--- script.js
+++ script.js
@@ -93,15 +93,68 @@ var getForecast = function(city) {
 };
 
 
-//display 5 day forecase
+// Displaying 5 day forecast   
+var displayForecast = function (list) { 
+    console.log(list);
 
-//date
+        for (var i = 0; i <= 4; i++) {
 
-//temp
+        //date
+        var displayDate1 = document.querySelector("#date-0");
+        var forecastDate1 = moment().add(1, "days").format("L");
+        displayDate1.textContent = forecastDate1;
 
-//humidity
+        var displayDate2 = document.querySelector("#date-1");
+        var forecastDate2 = moment().add(2, "days").format("L");
+        displayDate2.textContent = forecastDate2;
 
-//weather icons
+        var displayDate3 = document.querySelector("#date-2");
+        var forecastDate3 = moment().add(3, "days").format("L");
+        displayDate3.textContent = forecastDate3;
+
+        var displayDate4 = document.querySelector("#date-3");
+        var forecastDate4 = moment().add(4, "days").format("L");
+        displayDate4.textContent = forecastDate4;
+
+        var displayDate5 = document.querySelector("#date-4");
+        var forecastDate5 = moment().add(5, "days").format("L");
+        displayDate5.textContent = forecastDate5;
+
+        // temp
+        var displayTemp = document.querySelector(`#temp-${i}`);
+        var forecastTemp = list[i].main.temp + " °F";
+        displayTemp.textContent = forecastTemp; 
+
+        //humidity
+        var displayHumidity = document.querySelector(`#humidity-${i}`);
+        var forecastHumidity = list[i].main.humidity + "%";
+        displayHumidity.textContent = forecastHumidity;
+
+
+//icons (weather)
+var displayIcon1 = document.querySelector("#city-icon-1");
+var currentIcon1 = "https://openweathermap.org/img/wn/" + list[1].weather[0].icon + "@2x.png"
+displayIcon1.setAttribute ("src", currentIcon1);
+
+var displayIcon2 = document.querySelector("#city-icon-2");
+var currentIcon2 = "https://openweathermap.org/img/wn/" + list[2].weather[0].icon  + "@2x.png"
+displayIcon2.setAttribute ("src", currentIcon2);
+
+var displayIcon3 = document.querySelector("#city-icon-3");
+var currentIcon3 = "https://openweathermap.org/img/wn/" + list[3].weather[0].icon  + "@2x.png"
+displayIcon3.setAttribute ("src", currentIcon3);
+
+var displayIcon4 = document.querySelector("#city-icon-4");
+var currentIcon4 = "https://openweathermap.org/img/wn/" + list[4].weather[0].icon  + "@2x.png"
+displayIcon4.setAttribute ("src", currentIcon4);
+
+var displayIcon5 = document.querySelector("#city-icon-5");
+var currentIcon5 = "https://openweathermap.org/img/wn/" + list[5].weather[0].icon  + "@2x.png"
+displayIcon5.setAttribute ("src", currentIcon5);
+
+}
+
+};
 
 //search button
 
