4fd23f2a2550cca9d65d8f92add865f31bb9e65a
jjohnson673 <jkjohnson673@gmail.com>
2022-10-24 08:56:22 -0400

    added in APIs

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //API Key
-
+var key = "4d89fc5098647df5a30436dd979396e9";
 
 // DOM Elements to Display on page
 
@@ -10,17 +10,47 @@
 //previously searched city button clicks
 
 
-///requesting current weather for city API
+// requesting Current Weather API
+var getCityWeather = function(city) {
+    var apiURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + key;
+
+    // if response was successful 
+    fetch(apiURL).then(function(response) {
+        if (response.ok) {
+            response.json().then(function(data) {
+                displayCityWeather(data, city);
+            });
+        } else {
+            alert("Error:" + response.statusText);
+        }
+    })
+    // if network error 
+    .catch(function(error) {
+        alert("Unable to connect to Open Weather");
+    })
+
+}
 
 
-//if response was successful
+//UV index API 
+var searchCityUV = function(lon, lat, city) {
+    var uvUrl = "https://api.openweathermap.org/data/2.5/uvi?q=" + city + "&appid=" + key + "&lat=" + lat + "&lon=" + lon; 
 
-//if response was not successful/ network error
-
-
-//UV index API request
-
-//if error
+    fetch(uvUrl).then(function(response) {
+        if (response.ok) {
+            response.json().then(function(lon, lat, city) {
+                displayCurrentUv(lon, lat, city);
+            });
+        } else {
+            alert("Error:" + response.statusText);
+        }
+        })
+        
+        // if network error 
+        .catch(function(error) {
+            alert("Unable to connect to Open Weather");
+    })
+};
 
 
 //current weather data display
@@ -42,11 +72,25 @@
 
 //display UV
 
-//5 day forecast API
+// 5 day forecast API 
+var getForecast = function(city) {
+    var forecastURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial&cnt=6&appid=" + key;
 
-//is response successful
-
-//if network error
+    // if response was successful 
+    fetch(forecastURL).then(function(response) {
+        if (response.ok) {
+            response.json().then(function(data) {
+                displayForecast(data.list);
+            });
+        } else {
+            alert("Error:" + response.statusText);
+        }
+    })
+    // if network error 
+    .catch(function(error) {
+        alert("Unable to connect to Open Weather");
+    })
+};
 
 
 //display 5 day forecase
